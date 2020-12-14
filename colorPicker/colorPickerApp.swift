52138950828a950260519561cb2bbe6f7c6459dc
Hayyim <b2banalytica@gmail.com>
2020-12-14 12:31:58 +0300

    Initial Commit

---
+++ colorPicker/colorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorPickerApp.swift
+//  colorPicker
+//
+//  Created by vitasiy on 14.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct colorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
